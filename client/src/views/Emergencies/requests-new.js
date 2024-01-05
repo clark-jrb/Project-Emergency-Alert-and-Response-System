76373c492d6446d7614151e4531e14efc33ae987
izmeclark <98464661+izmeclark@users.noreply.github.com>
2024-01-06 02:10:05 +0800

    installed cors

--- client/src/views/Emergencies/requests-new.js
+++ client/src/views/Emergencies/requests-new.js
@@ -3,19 +3,20 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 const New = () => {
-    const [requests, setRequests] = useState(null)
+    const [requests, setRequests] = useState([])
     const hasRendered = useRef(false)
 
     useEffect(() => {
         if (!hasRendered.current) {
             const fetchRequests = async () => {
                 try {
-                    const response = await fetch('/usf/emergencies')
+                    const response = await fetch('http://localhost:4000/usf/emergencies')
 
+                    console.log(response)
                     if (response.ok) {
-                        // const data = await response.json()
-                        console.log(response)
-                        setRequests(response)
+                        const data = await response.json()
+                        // console.log(response)
+                        setRequests(data)
                         console.log('Data loaded!')
                     } else {
                         console.log('Error occurred')
@@ -39,7 +40,7 @@ const New = () => {
     return (
         <div className='request-new'>
             <h3>New</h3>
-            <button className='btn btn-success'>datas</button>
+            {/* <button className='btn btn-success'>datas</button> */}
         </div>
     )
 }
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 require('dotenv').config()
 const port = process.env.PORT
+const cors = require('cors')
 
 // Routes
 const usfRoutes = require('./routes/usfRoutes')
@@ -17,6 +18,7 @@ app.listen(port, () => {
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use(cors())
 
 // routes
 app.get('/', (req, res) => {
