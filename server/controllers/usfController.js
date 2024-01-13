abe9a1bb33b1b8d6fc90b5ec027c77eb0c3d4f12
izmeclark <98464661+izmeclark@users.noreply.github.com>
2024-01-13 23:47:37 +0800

    REALIZATIONS
    gonna use firebase inside react now

--- server/controllers/usfController.js
+++ server/controllers/usfController.js
@@ -4,53 +4,57 @@ const moment = require('moment')
 // set responder to the collection name in firestore
 const requests = db.collection('emergency_requests')
 
-const getRequests = async (req, res) => {
+// GET ALL REQUEST DATA
+const getRequests = (req, res) => {
     try {
-        // const snapshot = await requests.get()
-        // const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
-        const snapshot = await requests.get()
-        const data = snapshot.docs.map(doc => {
-            const { timestamp, ...rest } = doc.data()
-            const timepoint = timestamp.toDate() // Convert Firestore timepoint to JavaScript Date object
+        const unsubscribe = requests.onSnapshot((snapshot) => {
+        const data = snapshot.docs.map((doc) => {
+        const { timestamp, ...rest } = doc.data()
+        const timepoint = timestamp.toDate()
 
-            // Extract data and time components
-            const dataComponent = moment(timepoint).format('LL'); // Format: YYYY-MM-DD
-            const timeComponent = moment(timepoint).format('LT'); // Format: HH:mm:ss
+        const dataComponent = moment(timepoint).format('LL')
+        const timeComponent = moment(timepoint).format('LT')
 
-            return {
-                id: doc.id,
-                date: dataComponent,
-                time: timeComponent,
-                ...rest
-            }
+        return {
+            id: doc.id,
+            date: dataComponent,
+            time: timeComponent,
+            ...rest
+        }
         })
 
         res.json(data)
+    })
+
+      // Cleanup the listener when the request is completed
+    res.on('finish', () => {
+        unsubscribe()
+    })
     } catch (error) {
-        res.status(500).json({error: error.message});
+        res.status(500).json({ error: error.message })
     }
 }
 
 // UPDATE FUNCTION (UPDATE REQUEST STATUS)
 
 async function updateRequest(reqID, updatedData) {
-    const specRequest = requests.doc(reqID);
+    const specRequest = requests.doc(reqID)
     
     try {
-        const requestDoc = await specRequest.get();
+        const requestDoc = await specRequest.get()
     
         if (!requestDoc.exists) {
-            throw new Error('Request not found');
+            throw new Error('Request not found')
         }
     
         await specRequest.update({
             ...updatedData
-        });
+        })
     
-        return 'Request updated successfully';
+        return 'Request updated successfully'
     } catch (error) {
-        console.error('Error updating request:', error.message);
-        throw error;
+        console.error('Error updating request:', error.message)
+        throw error
     }
 }
 
