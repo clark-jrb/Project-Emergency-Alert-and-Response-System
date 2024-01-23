fcb8bce3bf490145541bbf6cc26de9c62700f387
izmeclark <98464661+izmeclark@users.noreply.github.com>
2024-01-23 18:04:21 +0800

    pagination button added

--- client/src/views/History/history-datas.js
+++ client/src/views/History/history-datas.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useRequestContext } from '../../context/RequestContext'
 
 const HistoryDatas = () => {
     const { requests } = useRequestContext()
+    const [currentPage, setCurrentPage] = useState(1)
+    const itemsPerPage = 10
+
+    const indexOfLastItem = currentPage * itemsPerPage
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage
+    const currentRequests = requests.slice(indexOfFirstItem, indexOfLastItem)
+
+    const paginate = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
+    
+    const nextPage = () => {
+        setCurrentPage((prevPage) => prevPage + 1);
+    };
+    
+    const prevPage = () => {
+        setCurrentPage((prevPage) => prevPage - 1);
+    };
 
     return (
         <div className='history-datas p-3'>
@@ -24,10 +42,14 @@ const HistoryDatas = () => {
                         </tr>
                     </thead>
                     <tbody>
-                        {requests.map((data) => (
+                        {currentRequests.map((data) => (
                             <tr key={data.id}>
                                 <td className='table-num px-1'>{data.req_no}</td>
-                                <td className='table-level'>{data.emergency_level}</td>
+                                <td className='table-level td-level px-2'>
+                                    <div className='td-level-con py-1'>
+                                        {data.emergency_level}
+                                    </div>
+                                </td>
                                 <td className='table-date'>{data.date}</td>
                                 <td className='table-time'>{data.time}</td>
                                 <td className='table-ET'>{data.emergency_type}</td>
@@ -37,6 +59,22 @@ const HistoryDatas = () => {
                         ))}
                     </tbody>
                 </table>
+                {/* Pagination */}
+                <div>
+                    <button onClick={prevPage} disabled={currentPage === 1}>
+                        Previous
+                    </button>
+
+                    {Array.from({ length: Math.ceil(requests.length / itemsPerPage) }).map((_, index) => (
+                    <button key={index} onClick={() => paginate(index + 1)} className={`px-2 ${currentPage === index + 1 ? 'active' : ''}`}>
+                        {index + 1}
+                    </button>
+                    ))}
+
+                    <button onClick={nextPage} disabled={currentPage === Math.ceil(requests.length / itemsPerPage)}>
+                        Next
+                    </button>
+                </div>
             </div>
         </div>
     )
