d3e0adf4081ee865d0af58ebf3ce910b425383b2
izmeclark <98464661+izmeclark@users.noreply.github.com>
2024-01-27 16:37:05 +0800

    messages UI tweaks

--- client/src/views/Messages/get-messages.js
+++ client/src/views/Messages/get-messages.js
@@ -3,8 +3,21 @@ import React from 'react'
 const GetMessages = () => {
     return (
         <div className='get-messages'>
-            <div className='message-person border'>
-                <p>This is for messages</p>
+            <div className='message-person-con container py-2'>
+                {/* Person Name Initials */}
+                <div className='name-logo'>
+                    <p className='m-0'>CJ</p>
+                </div>
+                {/* Message */}
+                <div className='message-info'>
+                    {/* Person Name */}
+                    <p className='m-0 person-name'>Clark John Busuego</p>
+                    {/* Person Message */}
+                    <div className='person-mess-cont d-flex'>
+                        <p className='m-0 person-message'>Need help need ko momol</p>
+                        <span className='timestamp'> • 55m</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
