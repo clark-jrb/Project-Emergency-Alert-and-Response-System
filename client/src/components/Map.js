19ebf5fea03e034e80bf876690af6351c890408c
izmeclark <98464661+izmeclark@users.noreply.github.com>
2024-02-12 23:58:33 +0800

    leaflet installed and rendered (map component)

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,11 +1,23 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { Icon } from 'leaflet';
+import '../styles/map.css'
 
 const Map = () => {
     return (
-        <div>
-            <h2>Map</h2>
-        </div>
+        <MapContainer center={[15.735976, 120.931406]} zoom={17} scrollWheelZoom={false}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+            />
+            <Marker position={[15.735976, 120.931406]}>
+                {/* <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup> */}
+            </Marker>
+        </MapContainer>
     )
 }
 
