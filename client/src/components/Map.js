5e4a01ca084fd9e5a253cefe8d6c448e2af4e692
izmeclark <98464661+izmeclark@users.noreply.github.com>
2024-02-13 01:42:28 +0800

    marker customized

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -6,16 +6,25 @@ import { Icon } from 'leaflet';
 import '../styles/map.css'
 
 const Map = () => {
+    const customIcon = new Icon({
+        iconUrl: require('../images/markers/marker-location1.png'),
+        iconSize: [38, 38],
+        iconAnchor: [12, 39],
+        shadowUrl: require('../images/markers/shadow.png'),
+        shadowSize: [15, 8],
+        shadowAnchor: [0.3, 5]
+    })
+
     return (
-        <MapContainer center={[15.735976, 120.931406]} zoom={17} scrollWheelZoom={false}>
+        <MapContainer center={[15.735976, 120.931406]} zoom={17}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
-            <Marker position={[15.735976, 120.931406]}>
-                {/* <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup> */}
+            <Marker position={[15.735718, 120.931453]} icon={customIcon}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
             </Marker>
         </MapContainer>
     )
