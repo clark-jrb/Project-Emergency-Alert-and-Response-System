d2cc57507f61d09981c98ed2e80114e19a910acf
izmeclark <98464661+izmeclark@users.noreply.github.com>
2024-02-14 00:31:22 +0800

    map marker tweaks

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -4,16 +4,40 @@ import 'leaflet/dist/leaflet.css'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
 import { Icon } from 'leaflet';
 import '../styles/map.css'
+import { useRequestContext } from '../context/RequestContext'
 
 const Map = () => {
-    const customIcon = new Icon({
-        iconUrl: require('../images/markers/marker-location1.png'),
-        iconSize: [38, 38],
-        iconAnchor: [12, 39],
-        shadowUrl: require('../images/markers/shadow.png'),
-        shadowSize: [15, 8],
-        shadowAnchor: [0.3, 5]
+    const { requests } = useRequestContext()
+
+    const L1accident = new Icon({
+        iconUrl: require('../images/markers/L1accident.png'),
+        iconSize: [69, 69],
+        iconAnchor: [32, 59],
+        popupAnchor: [3, -50]
     })
+
+    const L1assist = new Icon({
+        iconUrl: require('../images/markers/L1assist.png'),
+        iconSize: [69, 69],
+        iconAnchor: [32, 59],
+        popupAnchor: [3, -50]
+    })
+
+    const L1violence = new Icon({
+        iconUrl: require('../images/markers/L1violence.png'),
+        iconSize: [69, 69],
+        iconAnchor: [32, 59],
+        popupAnchor: [3, -50]
+    })
+
+    const L1security = new Icon({
+        iconUrl: require('../images/markers/L1security.png'),
+        iconSize: [69, 69],
+        iconAnchor: [32, 59],
+        popupAnchor: [3, -50]
+    })
+
+    // console.log(requests[0]);
 
     return (
         <MapContainer center={[15.735976, 120.931406]} zoom={17}>
@@ -21,11 +45,14 @@ const Map = () => {
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
             />
-            <Marker position={[15.735718, 120.931453]} icon={customIcon}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+            {requests.filter(request => request.status === 'New').map(request => (
+                <Marker key={request.id} position={[request.location.latitude, request.location.longitude]} icon={L1assist}>
+                    <Popup>
+                        A pop up.
+                    </Popup>
+                </Marker>
+            ))}
+            
         </MapContainer>
     )
 }
