faef84f0cfb681535cd012e573b2681b501c4c7b
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-02-25 00:38:56 +0800

    totals section

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,21 +2,78 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { useAuth } from '../context/AuthContext'
 import { useUsersContext } from '../context/UsersContext'
+import { useRequestContext } from '../context/RequestContext'
 import '../styles/dashboard.css'
+import Siren from '../images/logo/siren.png'
+import Ongoing from '../images/logo/ongoing.png'
+import Cancel from  '../images/logo/cancel.png'
+import Checked from '../images/logo/checked.png'
 
 const Dashboard = () => {
     const { currentUser } = useAuth()
     const { admins } = useUsersContext()
+    const { requests } = useRequestContext()
 
     const isResponder = admins.some(admin => admin.id === currentUser.uid);
+
+    let totalRequest = requests.length;
+    let completedRequest = requests.filter(request => request.status === 'Complete').length;
+    let ongoingRequest = requests.filter(request => request.status === 'Ongoing').length;
+    let canceledRequest = requests.filter(request => request.status === 'Declined').length;
+
+    console.log(completedRequest);
 
     return (
         <div className='dashboard-content p-4'>
             <div className='dashboard p-4 d-flex'>
                 <div className='adminProfile'></div>
                 <div className='forContent d-flex'>
-                    <div className='totals-section'></div>
-                    <div className='history-section'></div>
+
+                    <div className='totals-section d-flex px-4'>
+                        <div className='total-emergency d-flex p-4'>
+                            <div className='total-icon'>
+                                <img src={Siren} alt='siren-logo'/>
+                            </div>
+                            <div className='total-title'>
+                                <p className='m-0 total-count'>{totalRequest}</p>
+                                <p className='m-0'>Total Emergencies</p>
+                            </div>
+                        </div>
+
+                        <div className='total-ongoing d-flex p-4'>
+                            <div className='total-icon'>
+                                <img src={Ongoing} alt='ambulance-logo'/>
+                            </div>
+                            <div className='total-title'>
+                                <p className='m-0 total-count'>{ongoingRequest}</p>
+                                <p className='m-0'>Total On Going</p>
+                            </div>
+                        </div>
+
+                        <div className='total-complete d-flex p-4'>
+                            <div className='total-icon'>
+                                <img src={Checked} alt='checked-logo'/>
+                            </div>
+                            <div className='total-title'>
+                                <p className='m-0 total-count'>{completedRequest}</p>
+                                <p className='m-0'>Total Completed</p>
+                            </div>
+                        </div>
+
+                        <div className='total-canceled d-flex p-4'>
+                            <div className='total-icon'>
+                                <img src={Cancel} alt='cancel-logo'/>
+                            </div>
+                            <div className='total-title'>
+                                <p className='m-0 total-count'>{canceledRequest}</p>
+                                <p className='m-0'>Total Canceled</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='history-section'>
+
+                    </div>
                 </div>
             </div>
             {/* {currentUser.uid ? (
