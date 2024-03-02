e46b372b05912b55a1a22e743be91d110daa0396
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-03-02 17:55:39 +0800

    server ousted

--- server/routes/usersRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getUsers } = require('../controllers/UsersController')
-
-router.get('/', getUsers)
-
-module.exports = router
