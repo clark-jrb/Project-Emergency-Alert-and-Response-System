7d776c30b3ce9742c7047818ab28f053b41c6010
izmeclark <98464661+clark-jrb@users.noreply.github.com>
2024-05-10 00:02:29 +0800

    uncomment

--- client/src/utils/LogsAction.js
+++ client/src/utils/LogsAction.js
@@ -4,7 +4,7 @@ import { addDoc, collection } from 'firebase/firestore';
 
 export const logUserAction = async ( action, userData ) => {
     try {
-        console.log(userData);
+        // console.log(userData);
         await addDoc(collection(db, 'user_logs'), {
             // userId: userData.uid,
             displayName: userData.displayName,
